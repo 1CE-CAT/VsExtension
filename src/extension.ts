d61a585acfade3a78fd16fd1caac24acc784e1df
1CE-CAT <gisnet9@hotmail.com>
2026-03-15 15:46:59 +0500

    adding mdItEmoji

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,6 @@
 import * as vscode from 'vscode';
 import markdownItContainer from 'markdown-it-container';
+import { full as emoji } from 'markdown-it-emoji'
 
 export function activate(context: vscode.ExtensionContext) {
   return {
@@ -17,13 +18,16 @@ export function activate(context: vscode.ExtensionContext) {
         marker: '?',
         validate: () => true,
         render: (tokens: any, idx: number) => {
-          // Логика для скрытия/раскрытия
-          return tokens[idx].nesting === 1 ? 
-            '<div class="spoiler"><details><summary>' : 
-            '</summary></details></div>';
+          if (tokens[idx].nesting === 1) {
+            const title = tokens[idx].info.trim().replace(/^spoiler\s+/, '') || 'Спойлер';
+            return `<details class="spoiler"><summary>${title}</summary>\n`;
+          } else {
+            return '</details>\n';
+          }
         }
       });
-
+      
+      md.use(emoji);
       return md;
     }
   };
